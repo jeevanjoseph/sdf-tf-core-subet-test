28815f77976dadfecad0b40f380f745ea5cedffc
venugopal <venugopal.naik@oracle.com>
2019-07-26 11:43:16 +1000

    removing the terraform_config folder

--- terraform_config/simple/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "oci" {
-  version          = ">= 3.0.0"
-  tenancy_ocid     = "${var.tenancy_id}"
-  user_ocid        = "${var.user_id}"
-  fingerprint      = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
-  region           = "${var.region}"
-}
--- terraform_config/simple/variables.tf
+++
@@ -1,8 +0,0 @@
-
-variable "tenancy_id" {}
-variable "user_id" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
-variable "default_compartment_id" {}
-
